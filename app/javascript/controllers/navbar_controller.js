0fd851c497babdcef03d5391a079c5d8c3d43ea9
elise rochaix <elise.rochaix@gmail.com>
2023-07-05 11:30:27 +0200

    copy

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="navbar"
+export default class extends Controller {
+  connect() {
+  }
+
+  scrollTo(event) {
+    event.preventDefault();
+
+    const targetId = event.currentTarget.getAttribute("href");
+    const targetSection = document.querySelector(targetId);
+
+    if (targetSection) {
+      targetSection.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  }
+
+  redirectTo() {
+    window.location.href ="/";
+  }
+}
